6d2306c81922a8ef41a315ea4511f4b18bdebced
Ilya Sidorov <ilyasidorov114@gmail.com>
2020-03-20 11:34:19 +0300

    Create Spring Boot configuration

---
+++ src/main/java/com/iliasidorov/vinylstockmanager/VinylStockManagerApplication.java
@@ -0,0 +1,13 @@
+package com.iliasidorov.vinylstockmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VinylStockManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VinylStockManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iliasidorov/vinylstockmanager/VinylStockManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iliasidorov.vinylstockmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VinylStockManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
